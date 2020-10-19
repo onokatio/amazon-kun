7a421a667eaaf4c5703dc3155a5adbe8b3a26583
onokatio <onokatio@gmail.com>
2020-10-20 00:05:32 +0900

    init

---
+++ userscript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         あまぞんくん
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.amazon.co.jp/s?k=GABA
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    const getElementsById("s-asin")
+})();
